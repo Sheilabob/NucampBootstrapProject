2f89e3ad7ac6881a1973edb4087d6a7f79fac745
Sheila Strahan <smstrahan@gmail.com>
2021-10-16 10:28:17 -0600

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,13 @@ $(function() {
         }
     });
 
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+
+
 });
